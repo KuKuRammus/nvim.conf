0e475646c21868e754291b19135040732d7ba751
KuKuRammus <vladkamyshanov@gmail.com>
2025-07-08 19:28:00 +0300

    update

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -35,7 +35,7 @@ return {
 
                 format_on_save = {
                     -- These options will be passed to conform.format()
-                    timeout_ms = 5000,
+                    timeout_ms = 10000,
                     lsp_format = "fallback",
                 }
             })
