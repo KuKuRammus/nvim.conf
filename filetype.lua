9b2bf92faa47c10516bb7260a7aae523b4bcc201
KuKuRammus <vladkamyshanov@gmail.com>
2025-07-17 21:41:44 +0300

    update

--- filetype.lua
+++ filetype.lua
@@ -1,3 +1,15 @@
 -- File type detection rules
 -- Use `vim.filetype.add(...)` function to setup new file types detection rules
 -- See: https://neovim.io/doc/user/lua.html#vim.filetype.add()
+
+vim.filetype.add({
+    extension = {
+        -- C
+        c = "c",
+        h = "c",
+
+        -- C++
+        cpp = "cpp",
+        hpp = "hpp",
+    },
+})
