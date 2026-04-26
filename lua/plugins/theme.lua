5532d9507b2b328235b2dd36204ee15f0c552214
KuKuRammus <vladkamyshanov@gmail.com>
2026-04-27 02:37:06 +0300

    update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,8 @@
 -- Theme / Colors
 --
 
--- kanagawa.nvim
--- https://github.com/rebelot/kanagawa.nvim
-vim.pack.add({ "https://github.com/rebelot/kanagawa.nvim" })
-
-require("kanagawa").setup({ compile = true })
-
-vim.cmd.colorscheme("kanagawa-wave")
+-- onedark
+-- https://github.com/navarasu/onedark.nvim
+vim.pack.add({ "https://github.com/navarasu/onedark.nvim" })
+require("onedark").setup({ style = "warmer" })
+require("onedark").load()
